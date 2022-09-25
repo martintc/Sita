b837e6f16c599a360fe144ca1be409bf75847931
Todd Martin <kj4ntv@gmail.com>
2022-09-24 21:03:58 -0700

    Removed comments as I believe they were clashing with GCC's assembler

--- src/boot.s
+++ src/boot.s
@@ -1,31 +1,25 @@
 	.section ".text.boot"
 
-	.global _start // execution starts here
+	.global _start
 
 _start:
-	// check processor ID is zero
 	mrs	x1, mpidr_el1
 	and	x1, x1, #3
 	cbz	x1, 2f
-	// not on main core, so conduct inifinit loop
 1:	wfe
 	b	1b
-2:	// on main core
-
-	// set stack to start below code
+2:	
 	ldr	x1, =_start
 	mov	sp, x1
 
-	// clean the BSS section
-	ldr	x1, =__bss_start // start address
-	ldr	w2, =__bss_size // size of section
+	ldr	x1, =__bss_start
+	ldr	w2, =__bss_size
 3:	cbz	w2, 4f
 	str	xzr, [x1], #8
 	sub	w2, w2, #1
-	cbnz	w2, 3b // loop if non-zero
+	cbnz	w2, 3b
 
-	// jump to main routine in C
 4:	bl	main
-	// in case it does return halt the master core
+
 	b	1b
 	
